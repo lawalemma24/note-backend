9a0c0e7cc548949fa4389362fa945268992f5bc6
lawalemma24 <lawalemma24@gmail.com>
2026-01-05 06:40:52 +0100

    fixes

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors ({
-    origin: 'https://note-app-front-end-indol.vercel.app',
+    origin: 'http://localhost:5173',
 }))
 const PORT = process.env.PORT || 3000;
 
